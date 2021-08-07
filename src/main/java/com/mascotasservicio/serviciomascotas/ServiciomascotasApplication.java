b114b90ba6e0cd3f4beee12d090c3d878cf6fa09
Gmrp2210 <gmrp22@hotmail.com>
2021-08-07 16:02:48 -0600

    first commit

---
+++ src/main/java/com/mascotasservicio/serviciomascotas/ServiciomascotasApplication.java
@@ -0,0 +1,13 @@
+package com.mascotasservicio.serviciomascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciomascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciomascotasApplication.class, args);
+	}
+
+}
